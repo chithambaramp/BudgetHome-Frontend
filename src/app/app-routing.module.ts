837f566df6876baf5da6c5bbcf1248c42b1aa302
Chithambaram <chithambaram2495@gmail.com>
2026-08-12 18:28:08 +0530

    AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    // canMatch: [AuthGuard],
+    canMatch: [AuthGuard],
     children: [
       {
         path: 'expenses',
