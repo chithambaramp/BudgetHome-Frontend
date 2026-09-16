e4776b9088ccfa43170bdfec3d1a60a15f362430
Chithambaram <chithambaram2495@gmail.com>
2026-09-16 13:16:57 +0530

    Animated Title

--- src/app/core/component/header/header.component.ts
+++ src/app/core/component/header/header.component.ts
@@ -15,6 +15,7 @@ export class HeaderComponent implements OnInit {
 
   @Output() sidebarToggle = new EventEmitter<void>();
   isOpen = false;
+  animatedTitle: string = 'Ergonomics';
 
   ngOnInit(): void {
 
